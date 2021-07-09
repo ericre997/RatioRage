34d65a378a97dee10c3c55030c6f31c4a55d402f
Eric Reel <ericre997@gmail.com>
2021-07-09 00:08:14 -0700

    add baseline code for generating sets of equivalent and non-equivalent ratios

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,155 @@ import { DynamicTexture } from "@babylonjs/core/Materials/Textures/dynamicTextur
 import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
 
 
+
+export class Ratio {
+    public numerator: number;
+    public denominator: number;
+    
+    constructor(numerator: number, denominator: number) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+}
+
+function getRandomInteger(minValueInclusive : number, maxValueExclusive : number) : number {
+    return minValueInclusive + Math.trunc(Math.random() * (maxValueExclusive - minValueInclusive));
+}
+
+function getRandomValue(values : number[]) : number {
+    let idxChosen = getRandomInteger(0, values.length);
+    return values[idxChosen];
+}
+
+function removeValueFromArray(values: number[], exclude : number) : boolean {
+    const idxExclude = values.indexOf(exclude, 0);
+    if(idxExclude > -1) {
+        values.splice(idxExclude, 1);
+        return true;
+    }
+    return false;
+}
+
+function getSeedRatio() {
+    let primes = [1,2,3,5,7];
+
+    // get random ratio where numerator and denom are both primes
+    let ratio = new Ratio(getRandomValue(primes), getRandomValue(primes));
+
+    removeValueFromArray(primes, 1);
+
+    // multiply one or the other by another prime (might be one)
+    let numberToMux = getRandomInteger(0,3);
+    switch(numberToMux) {
+        case 0:
+            // do nothing
+            break;
+        case 1: 
+            removeValueFromArray(primes, ratio.denominator);
+            ratio.numerator *= getRandomValue(primes);
+            break;
+        case 2: 
+            removeValueFromArray(primes, ratio.numerator);
+            ratio.denominator *= getRandomValue(primes);
+            break;
+        default:
+            throw new Error("unexpected value during seedRatio mux");
+    }
+
+    return ratio;
+}
+
+
+function getEquivalentRatios(ratio : Ratio, numRatios : number, factors : number[]) : Ratio[] {
+    let equivalentRatios = new Array<Ratio>();
+
+    let unusedFactors = [];
+    for(let i = 0; i < factors.length; i++) {
+        unusedFactors.push(factors[i]);
+    }
+
+    numRatios = Math.min(numRatios, factors.length);
+
+    for(let i = 0; i < numRatios; i++) {
+        let idxFactor = getRandomInteger(0, unusedFactors.length);
+        let equivalentRatio = new Ratio(ratio.numerator * unusedFactors[idxFactor], ratio.denominator * unusedFactors[idxFactor]);
+        equivalentRatios.push(equivalentRatio);
+        unusedFactors.splice(idxFactor,1);
+    }
+
+    return equivalentRatios;
+}
+
+function isRatioPresentInArray(ratio : Ratio, array : Ratio[]) : boolean {
+    for(let i = 0; i < array.length; i++){
+        if(array[i].numerator === ratio.numerator &&
+           array[i].denominator === ratio.denominator) {
+               return true;
+           }
+    }
+    return false;
+}
+
+// TODO:  this number picking strategy is pretty crude.  Probably gonna need to fine tune it.
+function getNonEquivalentRatios(ratio : Ratio, numRatios : number) : Ratio[] {
+    let nonEquivalentRatios = new Array<Ratio>();
+    const minInclusive = 1;
+    const maxExclusive = 101;
+
+    // TODO: this will spin forever in degerate cases  
+    // consider adding additional checks to prevent this.
+    while(nonEquivalentRatios.length < numRatios) {
+        let num1 = getRandomInteger(minInclusive, maxExclusive);
+        let num2 = getRandomInteger(minInclusive, maxExclusive);
+
+        // maintain ordering of specified ratio
+        if((ratio.numerator > ratio.denominator && num1 < num2) ||
+        (ratio.numerator < ratio.denominator && num1 > num2)) {
+            [num1, num2] = [num2, num1];
+        }
+    
+        let proposed = new Ratio(num1, num2);
+
+        // if they are equivalent, try again
+        if(ratio.numerator * proposed.denominator === ratio.denominator * proposed.numerator) {
+            continue;
+        }       
+
+        // check that we aren't already using this ratio
+        if(isRatioPresentInArray(proposed, nonEquivalentRatios)) {
+            continue;
+        }
+
+        nonEquivalentRatios.push(proposed);
+    }        
+
+    return nonEquivalentRatios;
+}
+
+
+// TODO:  break all of this nonsense out into it's own support class.
+// TODO:  shuffle the arrays, use the first equivalent one as the 'target'.
+// 
+// TODO:  the seedRatio might need to be modified so that we don't wind up with things like 2/49.
+// TODO:  the nonEquivalent picking strategy should probably be modified to use adaptive ranges that are similar
+//        to the range represented by the equivalent fractions.
+
+// TODO:  add HUD to display these values for debugging purposes.
+
+var equivalentRatios;
+var nonEquivalentRatios;
+
+function generateRatios(){
+    let seedRatio = getSeedRatio();
+    let factors = [2,3,4,5,6,7,8,9,10];
+    
+    equivalentRatios = getEquivalentRatios(seedRatio, 3, factors);
+    equivalentRatios.push(seedRatio);
+
+    nonEquivalentRatios = getNonEquivalentRatios(seedRatio, 5);
+}
+
+
 function createPlayer(scene: Scene, env: Environment) {
     let playerSize = 1;
     let walkSpeed = 20 / 1000;  // units per millisecond
@@ -49,19 +198,11 @@ function createPlayer(scene: Scene, env: Environment) {
     return player;
  }
 
-/*
-function movePlayerTo(env : Environment, posX : number, posZ : number) {
-    let posY = env.groundMesh.getHeightAtCoordinates(posX, posZ);
-    
-    player.position = new Vector3(posX, posY + .5, posZ);
-    let surfaceNormal = env.groundMesh.getNormalAtCoordinates(posX, posZ);
-    let rotationAxis = Vector3.Cross(surfaceNormal, player.up).normalize();
-    let angle = Math.acos(Vector3.Dot(surfaceNormal, player.up));
-    player.rotate(rotationAxis, angle); 
-}
-*/
 
 /// begin code!
+
+// temp
+generateRatios();
 
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 const engine = new Engine(canvas);
@@ -103,22 +244,12 @@ let targetDecalManager = new TargetDecalManager(scene);
 
 function movePlayer() {
     // TODO:  easing
-    // TODO: change to cube.
-
+    // TODO:  allow for multiple waypoints (i.e. remove current waypoint if hit?)
+    // TODO:  adjust speed based on terrain?
     let waypoint = targetDecalManager.getNextWaypoint();
     if(waypoint) {
         player.updatePlayerPosition(env, waypoint.position, engine.getDeltaTime());
     }        
-
-    // if within d2 of next waypoint, remove waypoint
-    // get next waypoint
-    // if no next waypoint, return
-    // compute move point
-    // get height at move point
-    // adjust xy distance based on delta Z
-    // move player to new xyz
-    // adjust player rotation to tangent at new xyz
-
 }
 
 // set up click detection
